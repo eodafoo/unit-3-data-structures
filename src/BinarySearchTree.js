8ad9488efc3c161db454f8f1d126804ab8de6212
jgiang <jasminegiang@MacBook-Pro.local>
2015-06-03 19:20:03 -0700

    need to repull for BST fixes

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -5,14 +5,43 @@ function BinarySearchTree() {
 }
 
 BinarySearchTree.prototype.add = function(value) {
-	
+	if(value < this.value){
+		if(this.left != null){
+			this.left.add(value);
+		} else {
+			this.left = new BinarySearchTree(value);
+		}
+	} else {
+		if(this.right != null){
+			this.right.add(value);
+		} else {
+			this.right = new BinarySearchTree(value);
+		}
+	}
 }
 
 BinarySearchTree.prototype.contains = function(value) {
-
+	if(value === this.value){
+		return true;
+	} else if(value < this.value) {
+		if(this.left.contains(value)){
+			return true;
+		}
+	} else {
+		if(this.right.contains(value)){
+			return true;
+		}
+	}
+	return false;
 }
 
 // applies the callback in the order of depth first
 BinarySearchTree.prototype.depthFirstLog = function(callback) {
-
+	callback(this.value);
+	if(this.left != null){
+		this.left.depthFirstLog(callback);
+	}
+	if(this.right != null){
+		this.right.depthFirstLog(callback);
+	}
 }
