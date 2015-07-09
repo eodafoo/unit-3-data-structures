ab424e3987aaecf4cb826f1fbd5174e0971ad572
jgiang <jasminegiang@MacBook-Pro.local>
2015-07-08 18:35:48 -0700

    solve trie

--- src/Trie.js
+++ src/Trie.js
@@ -1,14 +1,35 @@
 // Create a prefix tree (words with common prefixes are under the same sequence of edges)
 function Trie() {
+    this.root = new Node();
+}
 
+function Node() {
+    this.children = {};
 }
 
 // Insert a new word
 Trie.prototype.insert = function(string) {
+    this.insertSingle(this.root, string, 0);
+}
 
+Trie.prototype.insertSingle = function(node, string, i) {
+    if (!node) {
+        node = new Node();
+    }
+    if (i !== string.length) {
+        node.children[string[i]] = this.insertSingle(node.children[string[i]], string, i+1); 
+    }
+    return node;
 }
 
 // Returns true or false, depending on whether the word/prefix is contained in the tree
 Trie.prototype.find = function(string) {
-
+    var curr = this.root;
+    for (var i = 0; i < string.length; i++) {
+        if (!(string[i] in curr.children)) {
+            return false;
+        }
+        curr = curr.children[string[i]];
+    }
+    return true;
 }
