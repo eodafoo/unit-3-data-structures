5f9226bc7cd802e83775541f0ca725f9e07b494b
Andy <acarl005@g.ucla.edu>
2015-10-08 10:07:33 -0700

    Update tree-tests.js

--- test/tree-tests.js
+++ test/tree-tests.js
@@ -30,4 +30,18 @@ describe('Tree', function() {
     tree.children[0].add(4);
     expect(tree.contains(4)).to.eql(true);
   });
+  
+  xit('should calculate height', function() {
+    tree.add(5);
+    tree.add(8);
+    var son = tree.children[0];
+    son.add(1);
+    son.add(4);
+    var daughter = tree.children[0];
+    daughter.add(10);
+    var grandson = son.children[1];
+    grandson.add(3);
+    expect(grandson.height()).to.be(1);
+    expect(tree.height()).to.be(3);
+  });
 });
