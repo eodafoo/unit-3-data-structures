a646731fd1de0ebb3412e6aecde5c23ef0886374
Andrew Carlson <acarl005@g.ucla.edu>
2015-10-11 17:18:11 -0700

    change the name of Set

--- src/Set.js
+++ src/Set.js
@@ -1,19 +1,19 @@
-function Set() {
+function MySet() {
 
 }
 
 // adds an object to the set
 // does not add an object to the set if object is already present
-Set.prototype.add = function (value) {
+MySet.prototype.add = function (value) {
 
 }
 
 // returns true if the value is contained in the set
-Set.prototype.contains = function (value) {
+MySet.prototype.contains = function (value) {
 
 }
 
 // removes value from the set if present
-Set.prototype.remove = function (value) {
+MySet.prototype.remove = function (value) {
 
 }
